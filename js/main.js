9697e1c480c81c47f825f464113add934e03da45
OghaleOsokpor <90363908+OghaleOsokpor@users.noreply.github.com>
2024-10-03 00:19:47 -0400

    initial

--- js/main.js
+++ js/main.js
@@ -1,1 +1,29 @@
 // Enter your code below.
+let timesClicked = 0;
+
+const resourcesSection = document.querySelector('.javascript-resources');
+const revealButton = document.getElementById('show-resources');
+
+revealButton.addEventListener('click', function() {
+    resourcesSection.classList.toggle('d-none');
+});
+
+resourcesSection.addEventListener('mouseenter', function(event) {
+    if (event.target.classList.contains('list-group-item')) {
+        event.target.classList.add('bold-text');
+    }
+}, true);
+
+resourcesSection.addEventListener('mouseleave', function(event) {
+    if (event.target.classList.contains('list-group-item')) {
+        event.target.classList.remove('bold-text');
+    }
+}, true);
+
+resourcesSection.addEventListener('click', function(event) {
+    if (event.target.classList.contains('list-group-item')) {
+        event.target.classList.add('italic-text');
+        timesClicked++;
+        event.target.innerText = '${event.target.innerText} (Clicked ${timesClicked} times)';
+    }
+});
